2466bcdc531fc98d726f3385725ab052930bb60f
hugomatheus <hg.targino@gmail.com>
2020-11-01 20:59:56 -0300

    Perfil do usuario - Salvando tokens no banco

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+import { container } from 'tsyringe';
+
+class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { token, password } = request.body;
+
+    const resetPasswordService = container.resolve(ResetPasswordService);
+    await resetPasswordService.execute({
+      token,
+      password,
+    });
+
+    return response.status(204).json();
+  }
+}
+
+export default ResetPasswordController;
