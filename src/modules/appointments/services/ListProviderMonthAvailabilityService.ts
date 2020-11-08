cdaf03fbfe0630ec8fd8d7e8608914df21793b68
hugomatheus <hg.targino@gmail.com>
2020-11-08 16:31:51 -0300

    Agendamento - Listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { getDate, getDaysInMonth } from 'date-fns';
 
 interface IRequest {
   provider_id: string;
@@ -27,8 +28,22 @@ class ListProviderMonthAvailabilityService {
     const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       { provider_id, month, year },
     );
-    console.log(appointments);
-    return [{ day: 1, available: false }];
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (value, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+      return { day, available: appointmentsInDay.length < 10 };
+    });
+
+    return availability;
   }
 }
 
